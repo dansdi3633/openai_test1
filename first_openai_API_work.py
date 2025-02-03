005199e6e61aea1d8211932ceb70b9aa5bd37967
Dan <dansdi3633@gmail.com>
2025-02-03 16:57:56 +0800

    change to gpt-4o-2024-11-20 and other model, try to see if lastest information can be used with API, unfortunately not feasible

--- first_openai_API_work.py
+++ first_openai_API_work.py
@@ -3,10 +3,13 @@ import getpass # 保密輸入套件
 api_key = getpass.getpass("請輸入金鑰：")
 client = OpenAI(api_key = api_key) # 建立 OpenAI 物件
 reply = client.chat.completions.create(
-    model = "gpt-3.5-turbo",
-    # model = "gpt-4",
+     model = "gpt-3.5-turbo",
+    # model = "gpt-4o",
+    # model = "gpt-4o-2024-11-20",
+    # model = "chatgpt-4o-latest",
+    # model = "gpt-4-turbo-preview",
     messages = [
-        {"role":"user", "content": "你住的地方很亮嗎？"}
+        {"role":"user", "content": "你知道截至今天2024年8月2日台灣的總統是誰嗎？"}
     ]
 )
 print(reply)
